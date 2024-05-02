30aec32ee5f3ff3b7b4376257b0f7372bc26518b
Joe Linstrum <joelinstrum@gmail.com>
2024-05-02 10:59:26 -0700

    initial commit

---
+++ content.js
@@ -0,0 +1,38 @@
+const removeAds = () => {
+  var skipBtn = document.querySelector("button.ytp-ad-skip-button");
+  if (skipBtn) skipBtn.click();
+  var ad = document.querySelector(".ytp-ad-player-overlay");
+  if (ad) {
+    var video = document.querySelector("video");
+    if (video) {
+      video.currentTime = video.duration;
+    }
+    if (skipBtn) {
+      skipBtn.click();
+    }
+  }
+};
+removeAds();
+
+// Function to create a MutationObserver
+function setupMutationObserver() {
+  // Create a MutationObserver instance
+  const observer = new MutationObserver((mutations) => {
+    // Disconnect the observer to avoid reacting to changes initiated by this function
+    observer.disconnect();
+
+    removeAds();
+
+    // Reconnect the observer after making changes
+    observer.observe(document.body, config);
+  });
+
+  // Define the configuration for the observer
+  const config = { childList: true, subtree: true };
+
+  // Start observing the body and its descendants for changes
+  observer.observe(document.body, config);
+}
+
+// Execute setupMutationObserver when the content script is injected
+setupMutationObserver();
